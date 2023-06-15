c584b41a075ee5c81822666bea8547c48e06c4ba
Илья Александрович <itrachyk@megar.ru>
2023-06-15 11:40:44 +0300

    init

--- terraform/install_cluster/variables.tf
+++ terraform/install_cluster/variables.tf
@@ -6,15 +6,10 @@ variable "yandex_folder_id" {
   default = "b1g52g3reusei01pv9mn"
 }
 
-variable "image_nat" {
-  default = "fd8evlqsgg4e81rbdkn7"
-}
-
-
 variable "image_vm" {
   default = "fd8evlqsgg4e81rbdkn7"
 }
 
 variable "token_ya" {
-  default = "t1.9euelZrMncmJy8aanI3NkYvPz5aUzu3rnpWajpOWi5KWk5aUnsuUzcaLx4nl8_d6EVpb-e8FRm4E_d3z9zpAV1v57wVGbgT9zef1656VmpCOzo2MnovGz8rMnZmWy5eU7_zF656VmpCOzo2MnovGz8rMnZmWy5eU.Go5GDdKAdDB6X3vWeKXYrhL9tcTgF2sofMnspwJyYGsaSd3ttCf_a3fW2d7Nr_1_xEDt726a1xxjB0Ahge3KBQ"
+  default = "***"
 }
